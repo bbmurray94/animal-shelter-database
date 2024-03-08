1eb9878c865d5f99fea82371e4baac7a90d37c24
Ben Murray <beckmurray94@gmail.com>
2024-03-08 11:03:57 -0500

    Adding Notes table and test data

---
+++ Test Data/notes-test-data.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE Notes;
+
+INSERT INTO Notes (dogId, note)
+VALUES (1, "Needs harness");
+
+INSERT INTO Notes (dogId, note)
+VALUES (1, "Pulls");
+
+INSERT INTO Notes (dogId, note)
+VALUES (2, "Door dasher");
